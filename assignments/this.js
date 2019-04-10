7cc2805c12c9c076ed637d9655861c28d34eed61
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-04-10 18:15:41 -0400

    this.js questions 2 - 4 and examples 2 - 4 added

--- assignments/this.js
+++ assignments/this.js
@@ -2,9 +2,9 @@
 * in your own words. explain the four principle for the "this" keyword below.
 *
 * 1. Window binding. When "this" is in the global scope it references the Window or Global Object.
-* 2. Implicit binding. When "this" is 
-* 3. Explicit binding
-* 4. new keyword
+* 2. Implicit binding. When the function is invoked, the obj name, i.e., left of the dot is the reference for "this".
+* 3. Explicit binding Using the methods call, apply (for arrays) or bind, the "this" reference can be specified.
+* 4. new keyword The new keyword creates an instance of a constructor and adds a "this" object to the constructor.
 *
 * write out a code example of each explanation above
 */
@@ -22,14 +22,43 @@ console.log(this);
 
 // code example for Implicit Binding
 
-const 
+const sythLord = {
+    firstName: 'Darth',
+    lastName: 'Vader',
+    fullName: function() {
+        return `${firstName} + " " + ${lastName}`
+    }
+}
+
+console.log(sythLord.fullName);
+
+
 
 // Principle 3
 
 // code example for New Binding
 
+function Person(name, age){
+    this.name = name;
+    this.age = age;
+}
 
+const darthvader = new Person('darthvader', 100);
+const lukeskywalker = new Person('lukeskywalker', 18);
+
+console.log(darthvader.age);
 
 // Principle 4
 
 // code example for Explicit Binding
+
+const spoiler = function() {
+    console.log("Luke, I am your " + this.relation);
+};
+
+const vader = {
+    relation: "father",
+    survival: "no"
+};
+
+spoiler.call(vader);
