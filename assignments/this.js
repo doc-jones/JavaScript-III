7367be519f45be1cb2a9f8f2d21c615c6e82eed7
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-04-10 17:15:12 -0400

    In this.js Question 1 and Principle 1 complete

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. Implicit to the Global Object stuff here.
-* 2. 
-* 3. 
-* 4. 
+* 1. Window binding. When "this" is in the global scope it references the Window or Global Object.
+* 2. Implicit binding. When "this" is 
+* 3. Explicit binding
+* 4. new keyword
 *
 * write out a code example of each explanation above
 */
@@ -13,13 +13,22 @@
 
 // code example for Window Binding
 
+function Person(name){
+    this.name = name;
+}
+console.log(this);
+
 // Principle 2
 
 // code example for Implicit Binding
 
+const 
+
 // Principle 3
 
 // code example for New Binding
+
+
 
 // Principle 4
 
